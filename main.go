ffc6f5f157b1b8157a814413ee40bfabadd93db6
kwesidev <kwesidev@gmail.com>
2016-06-10 10:54:21 +0200

    Added function to detect Display Resolution,and code refactoring.

--- main.go
+++ main.go
@@ -1,68 +1,81 @@
 package main
+
 //Random Wallpapers
 //William Kwasidev
-
-import ("fmt"
-		"os/exec"
-		"os"
-		"time"
-		"net/http"
-		"log"
-		"io"
-		"bufio"
-    "flag"
-
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"os/exec"
+	"strings"
+	"time"
 )
 
-const URL string = "https://source.unsplash.com/random"
-const FILENAME string = "/tmp/randompic"
-const DEFAULT time.Duration = time.Second * 30
+const (
+	URL      string        = "https://source.unsplash.com/random"
+	FILENAME string        = "/tmp/randompic"
+	DEFAULT  time.Duration = time.Second * 30
+)
+
+var SCREEN = Dimension()
+
+//gets dimension
+func Dimension() string {
+	command := "xdpyinfo | grep dimension | awk '{ print $2 }'"
+	dim, err := exec.Command("sh", "-c", command).Output()
+	if err != nil {
+		log.Fatal(err)
+
+	}
+	//remove line feed
+	return strings.Trim(string(dim), "\x0a")
+}
+
 //Downloads Image and saves it to /tmp
-func DownloadImg(url string){
-    resp,err := http.Get(URL)
-    if err != nil{
+func DownloadImg(url string) {
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	//buffer := make([]byte,1024)
+	f, err := os.Create(FILENAME)
+	if err != nil {
+		log.Fatal(err)
 
-    	log.Fatal("Failed to Download ... Problem with your internet connection")
-    	return
-    }
-   //buffer := make([]byte,1024)
-   f,err := os.Create(FILENAME)
-   if err != nil{
+		return
+	}
 
-   	log.Fatal(err)
-
-   	return
-   }
-
-   defer  resp.Body.Close()
-   writer := bufio.NewWriter(f)
-   reader := bufio.NewReader(resp.Body)
-   //copy download to file
-   written,err := io.Copy(writer,reader)
-
-   if err != nil{
-
-   	log.Fatal(err)
-   }
-   log.Printf("%d bytes Downloaded",written)
+	defer resp.Body.Close()
+	writer := bufio.NewWriter(f)
+	reader := bufio.NewReader(resp.Body)
+	//copy download to file
+	written, err := io.Copy(writer, reader)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("%d bytes Downloaded", written)
 
 }
 
-func main(){
- //how often it should change the Wallpaper
-  interval := flag.Duration("interval",DEFAULT,"-interval how often do you want to change the Wallpaper?")
-  flag.Parse()
-
-  //tick every 20seconds ,return a go channel
-	change := time.Tick(*interval);
+func main() {
+	interval := flag.Duration("interval", DEFAULT, "-interval ")
+	flag.Parse()
+	//tick every 30seconds ,return a go channel
+	change := time.Tick(*interval)
 	for now := range change {
-		DownloadImg(URL)
-		fmt.Println(now,"Setting new Wallpaper")
+		link := URL + "/" + SCREEN
+		DownloadImg(link)
+		fmt.Println(now, "Setting new Wallpaper")
 		//change background image
-		err := exec.Command("gsettings","set","org.gnome.desktop.background","picture-uri",fmt.Sprintf("file://%s",FILENAME)).Run()
+		err := exec.Command("gsettings", "set", "org.gnome.desktop.background", "picture-uri", fmt.Sprintf("file://%s", FILENAME)).Run()
 
 		//terminate if error occured
-		if err != nil{
+		if err != nil {
 
 			log.Fatal("Fail to set Wallpaper")
 		}
--- main_test.go
+++ main_test.go
@@ -1,21 +1,24 @@
 package main
-import ("testing"
-		"flag"
-		"os"
+
+import (
+	"flag"
+	"os"
+	"testing"
 )
 
 //test main function
-func TestMain(m *testing.M){
+func TestMain(m *testing.M) {
 
 	flag.Parse()
 	os.Exit(m.Run())
 
 }
+
 //benchmark download
-func BenchmarkDownloadImg(b*testing.B){
-   for n:=0 ; n < b.N; n++{
+func BenchmarkDownloadImg(b *testing.B) {
+	for n := 0; n < b.N; n++ {
 
 		DownloadImg("https://source.unsplash.com/random")
-   }
+	}
 
 }
